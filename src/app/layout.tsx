70d3d73e8c6aa21f8b634f11597b9096021a2d0f
Krzysztof Płonka <info@krzysztofplonka.pl>
2026-06-05 20:42:41 +0200

    themeColor in array

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,7 @@ export const metadata: Metadata = {
     canonical: SITE_URL,
   },
   keywords: ["blog", "one-blog", "full-stack blog", "nextjs blog"],
-  themeColor: "#ffffff",
+  themeColor: ['#000000', '#ffffff'],
   creator: "Akshay Moolya",
   openGraph: {
     url: SITE_URL,
